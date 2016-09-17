b4d37c93e470504982e802c4f4c42c9f951041d7
zhanggeng <m13716920635@163.com>
2016-09-17 08:29:55 +0800

    Initial Commit

---
+++ chatDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  chatDemo
+//
+//  Created by vd on 16/9/17.
+//  Copyright © 2016年 vd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ chatDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  chatDemo
+//
+//  Created by vd on 16/9/17.
+//  Copyright © 2016年 vd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
